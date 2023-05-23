ceb50b57bab53e994b8237e2d4c414a3d3299641
Mayur <mayur.kant.96@gmail.com>
2023-05-23 17:00:59 +0530

    main class 1st commit

--- NewProject/ViewController.swift
+++ NewProject/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("this is class 1")
+        print("this is main class")
         // Do any additional setup after loading the view.
     }
 
