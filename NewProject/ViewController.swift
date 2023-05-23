834a6f8b87538361a68fc8f671a86c9157c354ca
Mayur <mayur.kant.96@gmail.com>
2023-05-23 16:58:27 +0530

    commit 1 from branch chidl1

--- NewProject/ViewController.swift
+++ NewProject/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("this is class 1")
         // Do any additional setup after loading the view.
     }
 
